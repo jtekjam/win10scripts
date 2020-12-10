f86521f61a8cabdf0bcadb6fa39cdc1ce4a58ced
Josh <69834714+jtekjam@users.noreply.github.com>
2020-12-10 11:27:22 -0600

    bug fix

--- win10debloat-biz.ps1
+++ win10debloat-biz.ps1
@@ -14,7 +14,6 @@ $tweaks = @(
 	"RequireAdmin",
 
 	### External Program Setup
-	"InstallJtekProgs", #REQUIRED FOR OTHER PROGRAM INSTALLS!
 	"InstallFoxitPDF",
 	"Install7Zip",
 	"InstallVLC",
@@ -31,17 +30,6 @@ $tweaks = @(
 	"Restart"
 )
 
-
-Function InstallJtekProgs {
-	Write-Output "Installing Chocolatey"
-	Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-	choco install chocolatey-core.extension -y
-	Write-Output "Running O&O Shutup with Recommended Settings"
-	Import-Module BitsTransfer
-	Start-BitsTransfer -Source "https://raw.githubusercontent.com/jtekjam/win10scripts/master/ooshutup10.cfg" -Destination ooshutup10.cfg
-	Start-BitsTransfer -Source "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -Destination OOSU10.exe
-	./OOSU10.exe ooshutup10.cfg /quiet
-}
 
 Function InstallGIMP {
 	Write-Output "Installing GIMP"
